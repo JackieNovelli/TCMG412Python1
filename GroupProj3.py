35f74eb9688fd0ec064d43342f292606dd6f7eb6
DJWilliams22 <DJWilliams22@users.noreply.github.com>
2021-02-24 23:35:07 -0600

    Update GroupProj3.py

--- GroupProj3.py
+++ GroupProj3.py
@@ -8,7 +8,14 @@ def main():
         rawData = webUrl.read().decode('utf-8')
         with open("data.txt", "w+") as myfile:
             myfile.write(rawData)
+    
+    TOTAL = 0
+    YEAR = 0
 
+    for line in open("data.txt"):
+        items = line.split()
+        if len(items) < 9:
+            continue
 
 if __name__ == '__main__':
     main()
