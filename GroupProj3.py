a05111d123d061fc8e64148747056a498e2422fc
JackieNovelli <jackie.novelli@gmail.com>
2021-02-24 23:47:50 -0600

    Jackie's Part Fixes #2

--- GroupProj3.py
+++ GroupProj3.py
@@ -10,5 +10,13 @@ def main():
             myfile.write(rawData)
 
 
+        year = items[3].split(':'[0][-4:]
+        if year == "1995":
+            year = year + 1
+        total = total +1
+
+print("Logs since January 1995: " + str( YEAR))
+print("Total Logs: " + str( TOTAL))
+
 if __name__ == '__main__':
     main()
