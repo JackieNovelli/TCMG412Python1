c765395c30a7f57a1936ce5deea6e4db244b5cc9
JackieNovelli <jackie.novelli@gmail.com>
2021-03-21 16:12:20 -0500

    commit to main

--- Docker Project/app.py
+++ Docker Project/app.py
@@ -10,12 +10,21 @@ app = Flask(__name__)
 @app.route('/md5/<string>') 
 def md5(string):
     hash_obj = hashlib.md5(string.encode('utf-8')) 
-    response = jsonify({"MD5_String": hash_obj.hexdigest()})
-    return response
+#   response = jsonify({"MD5_String": hash_obj.hexdigest()})
+#   return response
+    return jsonify(
+        input=string
+        output=hash_obj.hexdigest()
+    )
  
 @app.route('/factorial/<integer>') 
 def factorial(integer):	
-    return jsonify({"Factorial_Num": math.factorial(int(integer))})
+#   return jsonify({"Factorial_Num": math.factorial(int(integer))})
+    return jsonify(
+        input=integer
+        output=math.factorial(int(integer)
+    )
+
 
 @app.route('/fibonacci/<integer>')
 def fibonacci(integer):
@@ -26,20 +35,40 @@ def fibonacci(integer):
         num = Fib_list[-2]+Fib_list[-1]
         if num < n:
             Fib_list.append(num)
-    return jsonify({"Fib_List": Fib_list})
+#   return jsonify({"Fib_List": Fib_list})
+    return jsonify(
+        input=integer
+        output=Fib_list
+    )
 
 @app.route('/is-prime/<integer>')
 def is_prime(integer):
     integer = int(integer)
     if integer==1:
-        return jsonify({"Boolean_Value": "False"})
+#       return jsonify({"Boolean_Value": "False"})
+        return(
+            input=integer
+            output="False"
+        )
     elif (integer==2):
-        return jsonify({"Boolean_Value": "True"})
+#       return jsonify({"Boolean_Value": "True"})
+        return(
+            input=integer
+            output="True"
+        )
     else:
         for x in range(2,integer):
             if (integer % x==0):
-                return jsonify({"Boolean_Value": "False"})
-    return jsonify({"Boolean_Value": "True"})
+#               return jsonify({"Boolean_Value": "False"})
+                return(
+                    input=integer
+                    output="False"
+                )
+#   return jsonify({"Boolean_Value": "True"})
+    return(
+        input=integer
+        output="True"
+    )
 
 @app.route('/slack-alert/<string>')
 def slack_alert(string):
@@ -47,9 +76,17 @@ def slack_alert(string):
     url = 'https://hooks.slack.com/services/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'
     result = str(requests.post(url, json={'text': string, 'channel': "group-7"}))
     if result.find('200') != -1:
-        return jsonify({"Boolean_Value": "True"})
+#       return jsonify({"Boolean_Value": "True"})
+        return jsonify(
+            input=string
+            output="True"
+        )
     else:
-        return jsonify({"Boolean_Value": "False"})
+#       return jsonify({"Boolean_Value": "False"})
+        return jsonify(
+            input=string
+            output="False"
+        )
 		
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5000)
